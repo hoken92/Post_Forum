63d94868138bde96b78a5ad6f17484ffac57c87a
hoken92 <hkenneth92@gmail.com>
2024-10-13 20:45:17 -0700

    added new post modal

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ import * as getPosts from "./Posts/getPosts.js";
 const postContainer = document.getElementById("post-container");
 const pageContainer = document.getElementById("pagination-wrapper");
 
+const newPostModal = document.getElementById("post-modal");
+const postModal = new bootstrap.Modal("#staticBackdrop");
+
 // Loads the first page with 10 posts
 let page = 1;
 getPosts.loadPosts(page);
@@ -15,4 +18,14 @@ pageContainer.addEventListener("click", function (evt) {
     const currentPage = Number(evt.target.value);
     getPosts.loadPosts(currentPage);
   }
+});
+
+newPostModal.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+
+  console.log(document.getElementById("new-title").value);
+  console.log(document.getElementById("new-name").value);
+  console.log(document.getElementById("new-content").value);
+
+  postModal.hide();
 });
