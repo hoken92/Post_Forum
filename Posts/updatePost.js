65c10d91d54aa1216cd09c971b6c6f354eb277df
hoken92 <hkenneth92@gmail.com>
2024-10-14 20:18:08 -0700

    moved update function into a module

--- Posts/updatePost.js
+++ Posts/updatePost.js
@@ -1,3 +1,56 @@
+export function updateRatings(evt) {
+  evt.stopPropagation();
+
+  // Checks to see if the img icon is the target
+  if (evt.target.localName === "img") {
+    const postID =
+      evt.target.parentElement.previousElementSibling.getAttribute("id");
+
+    if (evt.target.alt === "likes") {
+      const likeEl = evt.target.previousSibling;
+      let likeAmount = likeEl.textContent;
+
+      // If post is liked, it will remove the like and added count
+      if (likeEl.classList.contains("fw-bold")) {
+        likeEl.classList.remove("fw-bold");
+        likeEl.classList.remove("text-success");
+        likeAmount--;
+
+        updateLikes(postID, likeAmount).then((likes) => {
+          likeEl.textContent = likes;
+        });
+      } else if (!likeEl.classList.contains("fw-bold")) {
+        likeEl.classList.add("fw-bold");
+        likeEl.classList.add("text-success");
+        likeAmount++;
+        updateLikes(postID, likeAmount).then((likes) => {
+          likeEl.textContent = likes;
+        });
+      }
+    } else if (evt.target.alt === "dislikes") {
+      const dislikeEl = evt.target.previousSibling;
+      let dislikeAmount = dislikeEl.textContent;
+
+      if (dislikeEl.classList.contains("fw-bold")) {
+        dislikeEl.classList.remove("fw-bold");
+        dislikeEl.classList.remove("text-danger");
+        dislikeAmount++;
+
+        updateDislikes(postID, dislikeAmount).then((dislikes) => {
+          dislikeEl.textContent = dislikes;
+        });
+      } else if (!dislikeEl.classList.contains("fw-bold")) {
+        dislikeEl.classList.add("fw-bold");
+        dislikeEl.classList.add("text-danger");
+        dislikeAmount--;
+        updateDislikes(postID, dislikeAmount).then((dislikes) => {
+          dislikeEl.textContent = dislikes;
+        });
+      }
+    }
+  }
+}
+
 export async function updateLikes(id, value) {
   const updateObj = {
     reactions: {
--- script.js
+++ script.js
@@ -2,6 +2,8 @@ import * as getPosts from "./Posts/getPosts.js";
 import * as createPosts from "./Posts/createPost.js";
 import * as updatePosts from "./Posts/updatePost.js";
 
+const searchForm = document.getElementById("search-form");
+
 const postContainer = document.getElementById("post-container");
 const pageContainer = document.getElementById("pagination-wrapper");
 
@@ -58,57 +60,14 @@ newPostModal.addEventListener("submit", function (evt) {
   }
 });
 
+// Search for post by text
+searchForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+});
+
+// Ability to like or dislike a post
 postContainer.addEventListener("click", function (evt) {
-  evt.stopPropagation();
-
-  // Checks to see if the img icon is the target
-  if (evt.target.localName === "img") {
-    const postID =
-      evt.target.parentElement.previousElementSibling.getAttribute("id");
-
-    if (evt.target.alt === "likes") {
-      const likeEl = evt.target.previousSibling;
-      let likeAmount = likeEl.textContent;
-
-      // If post is liked, it will remove the like and added count
-      if (likeEl.classList.contains("fw-bold")) {
-        likeEl.classList.remove("fw-bold");
-        likeEl.classList.remove("text-success");
-        likeAmount--;
-
-        updatePosts.updateLikes(postID, likeAmount).then((likes) => {
-          likeEl.textContent = likes;
-        });
-      } else if (!likeEl.classList.contains("fw-bold")) {
-        likeEl.classList.add("fw-bold");
-        likeEl.classList.add("text-success");
-        likeAmount++;
-        updatePosts.updateLikes(postID, likeAmount).then((likes) => {
-          likeEl.textContent = likes;
-        });
-      }
-    } else if (evt.target.alt === "dislikes") {
-      const dislikeEl = evt.target.previousSibling;
-      let dislikeAmount = dislikeEl.textContent;
-
-      if (dislikeEl.classList.contains("fw-bold")) {
-        dislikeEl.classList.remove("fw-bold");
-        dislikeEl.classList.remove("text-danger");
-        dislikeAmount++;
-
-        updatePosts.updateDislikes(postID, dislikeAmount).then((dislikes) => {
-          dislikeEl.textContent = dislikes;
-        });
-      } else if (!dislikeEl.classList.contains("fw-bold")) {
-        dislikeEl.classList.add("fw-bold");
-        dislikeEl.classList.add("text-danger");
-        dislikeAmount--;
-        updatePosts.updateDislikes(postID, dislikeAmount).then((dislikes) => {
-          dislikeEl.textContent = dislikes;
-        });
-      }
-    }
-  }
+  updatePosts.updateRatings(evt);
 });
 
 // Displays a toast with a message
